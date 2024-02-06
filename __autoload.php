9bd7d76af365afd8a3e8316b9892491f15de448e
Adan Vasconcelos <avasc007@fiu.edu>
2024-02-05 21:25:31 -0500

    fix autoload

--- __autoload.php
+++ __autoload.php
@@ -1,8 +1,8 @@
 <?php
-function __autoload($Name) {
+function myAutoLoad($Name) {
     $File = 'lib/' . str_replace('\\', DIRECTORY_SEPARATOR, $Name) . '.php';
     include_once($File);
 }
 
-spl_autoload_register(__NAMESPACE__ . '\__autoload');
+spl_autoload_register(__NAMESPACE__ . '\myAutoLoad');
 ?>
